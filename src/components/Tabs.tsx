9117b298a02c7ba3d79b4f7b00b58b9d531ecc3e
Shreshth Purohit <shreshthpurohit@gmail.com>
2024-10-10 23:40:22 +0530

    changes to services section for mobile view

--- src/components/Tabs.tsx
+++ src/components/Tabs.tsx
@@ -1,10 +1,12 @@
 import {Dispatch, SetStateAction} from "react";
 import {StaticImageData} from "next/image";
+import {ChevronLeftIcon, ChevronRightIcon} from "@heroicons/react/24/outline";
 
 
 function classNames(...classes: string[]) {
     return classes.filter(Boolean).join(' ')
 }
+
 
 export interface TabItem {
     title: string
@@ -14,39 +16,45 @@ export interface TabItem {
 }
 
 
-export default function Tabs({items, current, setCurrent}: {items: TabItem[], current: string, setCurrent: Dispatch<SetStateAction<string>> }) {
+export default function Tabs({items, current, setCurrent}: {items: TabItem[], current: TabItem, setCurrent: Dispatch<SetStateAction<TabItem>> }) {
+
+    const prev = () => {
+        if (items.indexOf(current) === 0) {
+            setCurrent(items[items.length - 1])
+            return
+        }
+        setCurrent(items[items.indexOf(current) - 1])
+    }
+    const next = () => {
+        if (items.indexOf(current) === items.length - 1) {
+            setCurrent(items[0])
+            return
+        }
+        setCurrent(items[items.indexOf(current) + 1])
+    }
+
+
     return (
         <div>
-            <div className={'block lg:hidden'}>
-                <label htmlFor="tabs" className="sr-only">
-                    Select a tab
-                </label>
-                <select
-                    id="tabs"
-                    name="tabs"
-                    defaultValue={current}
-                    className="block w-full rounded-md border-gray-300 focus:border-indigo-500 focus:ring-indigo-500"
-                >
-                    {items.map((tab) => (
-                        <option key={tab.title}>{tab.title}</option>
-                    ))}
-                </select>
-            </div>
             <div className="">
-                <nav aria-label="Tabs" className="flex space-x-4">
-                    {items.map((tab) => (
+                <nav aria-label="Tabs" className="flex space-x-4 overflow-x-auto no-scrollbar justify-between px-4 items-center">
+                    <button onClick={prev}>
+                        <ChevronLeftIcon className={'h-8 stroke-gray-500'}/>
+                    </button>
                         <button
-                            onClick={() => setCurrent(tab.title)}
-                            key={tab.title}
-                            aria-current={current === tab.title  ? 'page' : undefined}
+                            onClick={() => setCurrent(current)}
+                            key={current.title}
+                            aria-current={current === current  ? 'page' : undefined}
                             className={classNames(
-                                tab.title === current ? 'bg-indigo-100 text-indigo-700' : 'text-gray-500 hover:text-gray-700',
-                                'rounded-md px-3 py-2 text-sm font-medium',
+                                current === current ? '  text-stepnex-blue' : 'text-gray-500 hover:text-gray-700',
+                                'rounded-md px-3 py-2 text-md font-medium  text-nowrap',
                             )}
                         >
-                            {tab.title}
+                            {current.title}
                         </button>
-                    ))}
+                    <button onClick={next}>
+                        <ChevronRightIcon  className={'h-8 stroke-gray-500'}/>
+                    </button>
                 </nav>
             </div>
         </div>
